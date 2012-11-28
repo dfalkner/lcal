e10195722d0931855bf5aef8976ae526788ac6d6
D. Falkner <dfalkner@surgeworks.com>
2012-11-27 21:09:50 -0700

    added gems and trying to get heroku setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcal::Application.config.secret_token = 'aa5f734bbd3498996b43992f36216f629819054e9492034acbaa4c22baf0d24561bb4fbbd72ced6a397bbd259b27e565c0254ecee878fc5e6dec98cbee63d23a'
